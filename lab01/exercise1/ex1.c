49536bd91062d8aadf7bb863fb2ed290a68a3a30
itsubaidrehman <ubaidrehman627926@gmail.com>
2024-06-12 20:48:02 +0500

    added ex1 code

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -27,25 +27,27 @@ Each sequence will end with a NULL terminator and will have up to 20 nucleotides
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurances */
-    dna_seq -> A_count = num_occurrences(dna_seq -> sequence, 'A');
-    dna_seq -> C_count = num_occurrences(dna_seq -> sequence, 'C');
-    dna_seq -> G_count = num_occurrences(dna_seq -> sequence, 'G');
-    dna_seq -> T_count = num_occurrences(dna_seq -> sequence, 'T');
+    
 
-    // while (*(dna_seq -> sequence) != '\0') {
-	// if (dna_seq -> sequence == "A") {
-	// 	dna_seq -> A_count += 1;
-	// }
+	char *seq = dna_seq -> sequence;
 
-	// if (dna_seq -> sequence == "C") {
-	// 	dna_seq -> C_count += 1;
-	// }
-	// if (dna_seq -> sequence == "G") {
-	// 	dna_seq -> G_count += 1;
-	// }
-	// if (dna_seq -> sequence == "T") {
-	// 	dna_seq -> T_count += 1;
-	// }
-	// (dna_seq -> sequence)++;
-        //return dna_seq;
+    while (*seq != '\0') {
+		if (*seq == 'A') {
+			dna_seq -> A_count += 1;
+		}
+
+		if (*seq == 'C') {
+			dna_seq -> C_count += 1;
+		}
+
+		if (*seq == 'G') {
+			dna_seq -> G_count += 1;
+		}
+		if (*seq == 'T') {
+			dna_seq -> T_count += 1;
+		}
+		
+	    seq++;
+	}
+      
 }
