4ac08afb47b43f4832c68e4cac888bb32301e0a7
itsubaidrehman <ubaidrehman627926@gmail.com>
2024-06-13 22:34:51 +0500

    Added ll_cycle.c

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,20 @@
 #include <stddef.h>
 #include "ll_cycle.h"
+#include <stdio.h>
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node *fast_ptr = head;
+    node *slow_ptr = head;    // starting fast and slow ptr at the head of the list
+
+   // check if advacning fast ptr by 2 nodes results in NULL then list is cyclic else advance the fast ptr to new node
+   while (fast_ptr != NULL && fast_ptr -> next != NULL) {
+      fast_ptr = fast_ptr -> next -> next;
+      slow_ptr = slow_ptr -> next;
+
+      if (fast_ptr == slow_ptr) {
+         return 1;
+      }
+   }
+   
 }
